cbb7ad76d142a49eb90b2b3fc897473e7c991bbf
JulioCesar-A <juliocesar02e@gmail.com>
2024-10-28 12:10:18 -0300

    Classes Requisição de Criação - Cadastro/Renovação

---
+++ backend/app/schemas/cadastro.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, model_validator, Field
+from typing import List, Optional
+from datetime import date, timedelta
+from .enums import StatusGerais, NomeAnexos
+
+def get_data_atual():
+    return date.today()
+
+def get_data_mais_um_ano():
+    return get_data_atual() + timedelta(days=365)
+
+
+
+# Classes de Requisição para criação dos Cadastros e Renovações
+class CadastroCreateRequest(BaseModel):
+    data_cadastro : date = Field(default_factory=get_data_atual)
+
+class RenovacaoCreateRequest(BaseModel):
+    data_renovacao : date = Field(default_factory=get_data_atual)
+    data_expiracao : date = Field(default_factory=get_data_mais_um_ano)
+
