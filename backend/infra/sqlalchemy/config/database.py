af976ec828268b5c0bcb50fe25bc41478da4f5f7
JulioCesar-A <juliocesar02e@gmail.com>
2024-11-11 19:16:08 -0300

    Tratamento de erro de conexão

--- backend/infra/sqlalchemy/config/database.py
+++ backend/infra/sqlalchemy/config/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from contextlib import asynccontextmanager
+from sqlalchemy.exc import OperationalError
+from fastapi import HTTPException
 
 # Variáveis de conexão com banco de dados (teste)
 
@@ -33,6 +36,10 @@ def criar_db():
 
 
 # Função de dependência
+@asynccontextmanager
 async def get_db():
-    async with AsyncSessionLocal() as session:
-        yield session
+    try:
+        async with AsyncSessionLocal() as session:
+            yield session
+    except OperationalError as e:
+        raise HTTPException(status_code=503, detail="Erro ao conectar ao banco de dados. O serviço se encontra indisponível")
