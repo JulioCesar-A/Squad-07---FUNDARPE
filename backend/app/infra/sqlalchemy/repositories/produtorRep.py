2e37d10b2870bc073ee0b0b06a8b382995988839
JulioCesar-A <juliocesar02e@gmail.com>
2024-11-17 16:11:56 -0300

    Criação da base da função de inserção de pessoa física

---
+++ backend/app/infra/sqlalchemy/repositories/produtorRep.py
@@ -0,0 +1,89 @@
+from sqlalchemy.exc import IntegrityError
+from fastapi import HTTPException
+from schemas import schemas
+from ..models import models
+from datetime import date
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+class RepositorioProdutor():
+
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def inserir_produtor_pessoa_fisica(self, dados_produtor: schemas.ProdutorPessoaFisicaCreateRequest):
+        try:
+            # Verificar se o produtor já existe (via CPF)
+            query = select(models.ProdutorPessoaFisica).where(models.ProdutorPessoaFisica.cpf == dados_produtor.cpf)
+            result = await self.db.execute(query)
+            produtor_existente = result.scalar()
+
+            if produtor_existente:
+                raise HTTPException(
+                    status_code=400,
+                    detail="Um produtor com este CPF já está registrado"
+                )
+
+            # Criando instâncias com dados pré-validados para cada tabela do banco de dados
+            produtor = models.ProdutorCultural(
+            
+                id = "123456",  # Implementar lógica de criação de IDs
+            
+                email = dados_produtor.email,
+                senha = dados_produtor.senha, # Implementar Hashing da senha com bcrypt
+            
+                ativo = True
+            
+            )
+            
+            self.db.add(produtor)
+
+            pessoa_fisica = models.ProdutorPessoaFisica(
+            
+                id = produtor.id,
+            
+                nome = dados_produtor.nome_completo,
+                cpf = dados_produtor.cpf,
+                data_nasc = dados_produtor.data_nascimento
+            
+            )
+            
+            self.db.add(pessoa_fisica)
+
+            endereco = models.Endereco(
+                
+                id = produtor.id,
+
+                logradouro = dados_produtor.endereco.logradouro,
+                numero = dados_produtor.endereco.numero,
+                bairro = dados_produtor.endereco.bairro,
+                cidade = dados_produtor.endereco.cidade,
+                estado = dados_produtor.endereco.estado,
+                cep = dados_produtor.endereco.cep
+            
+            )
+
+            self.db.add(endereco)
+
+            cadastro = models.Cadastro(
+                
+                id_produtor = produtor.id,
+                
+                data_cadastro = date.today()
+            
+            )
+
+            self.db.add(cadastro)
+            
+            # Commit e refresh da instância recém-adicionada
+            await self.db.commit()
+            await self.db.refresh(produtor)
+
+            return {"message": "Produtor cultural pessoa física inserido com sucesso!"}
+
+        except IntegrityError:
+            await self.db.rollback()
+            raise HTTPException(
+                status_code=400,
+                detail="Erro de integridade: dados duplicados ou incorretos"
+            )
