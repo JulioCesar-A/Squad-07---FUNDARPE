542e781a1e24ee555d0e48a19e1588e9638e3bcf
JulioCesar-A <juliocesar02e@gmail.com>
2024-11-28 16:09:59 -0300

    Navegação com parâmetro de tipo para o formulário de cadastro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
-import Select from './pages/Cadastro/Select/Select';
+import Select from './pages/Forms/Select';
 import AboutSection from './pages/Home/AboutSection/AboutSection';
 import EditalSection from './pages/Home/EditalSection/EditalSection';
+import Cadastro from './pages/Forms/Cadastro/Cadastro';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/select" element={<Select />} />
         <Route path="/sobre" element={<AboutSection />} /> {/* Atualizado */}
         <Route path="/editais" element={<EditalSection />} /> {/* Atualizado */}
+        <Route path="/cadastro/:tipo" element={<Cadastro />} /> {/* Atualizado */}
       </Routes>
       <Footer />
     </Router>
